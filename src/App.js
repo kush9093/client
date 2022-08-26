d313f7d7b6363e31ae501958d5d835eafb688256
kush9093 <0bbd09@gmail.com>
2022-08-26 10:24:46 +0900

    temporary commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Index from './component';
 import Login from './component/login';
 import Register from "./component/register";
 import { Container } from 'react-bootstrap';
+import AccountAPI from './component/servise/accountAPI';
+import { useState } from 'react';
 
+// cmd ==> ipconfig
+const accountAPI = new AccountAPI("http:192.168.4.97:8080")
 
 function App() {
 
+    const [logon,setLogon] = useState(false);
 
   return (
     <BrowserRouter>
