55010eab54bb90500c05893e19e5ccb9c649ec5a
kush9093 <0bbd09@gmail.com>
2022-08-26 17:27:13 +0900

    2022-08-26 17:27

--- src/component/write.jsx
+++ src/component/write.jsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
 
 function Write() {
+    const subref = useRef({});
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
+        console.log(subref.current);
 
     }
 
@@ -11,35 +14,47 @@ function Write() {
         <form className="mt-5" onSubmit={handleSubmit}>
             <div className="h5">소비내역기입</div>
             <div className="form-floating mb-3">
-                <input type="date" className="form-control" id="itemDate" />
+                <input type="date" className="form-control" name="itemDate" ref={(elm)=>{
+                    subref.current["itemDate"] = elm;
+                }} id="itemDate" />
                 <label htmlFor="itemDate">소비날짜</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="text" className="form-control" id="useDesc" />
+                <input type="text" className="form-control" id="useDesc" name="useDesc" ref={(elm)=>{
+                    subref.current["useDesc"] = elm;
+                }} />
                 <label htmlFor="useDesc">사용내역</label>
             </div>
             <div className="row g-2 mb-3">
                 <div className="col-md">
                     <div className="form-floating">
-                        <input type="text" className="form-control" id="cashAmt" />
+                        <input type="text" className="form-control" id="cashAmt" name="cashAmt" ref={(elm)=>{
+                    subref.current["cashAmt"] = elm;
+                }} />
                         <label htmlFor="cashAmt">현금</label>
                     </div>
                 </div>
                 <div className="col-md">
                     <div className="form-floating">
-                        <input type="text" className="form-control" id="cardAmt" />
+                        <input type="text" className="form-control" id="cardAmt" name="cardAmt" ref={(elm)=>{
+                    subref.current["cardAmt"] = elm;
+                }} />
                         <label htmlFor="cardAmt">카드</label>
                     </div>
                 </div>
             </div>
             <div className="form-floating mb-3">
-                <select type="text" className="form-control" id="cateroy" >
+                <select type="text" className="form-control" id="cateroy" name="cateroy" ref={(elm)=>{
+                    subref.current["cateroy"] = elm;
+                }} >
 
                 </select>
                 <label htmlFor="cateroy">카테고리</label>
             </div>
             <div className="form-floating mb-3">
-                <input type="text" className="form-control" id="tag" />
+                <input type="text" className="form-control" id="tag" name="tag" ref={(elm)=>{
+                    subref.current["tag"] = elm;
+                }} />
                 <label htmlFor="tag">태그</label>
             </div>
             <div className="d-flex justify-content-end">
